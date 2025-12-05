ed4c62fce925460baa60debdc3bd7750565306cf
yumauda <firststep-yumauda@gmail.com>
2025-12-05 15:34:35 +0900

    headerとfooter

--- footer.php
+++ footer.php
@@ -1,6 +1,67 @@
-<footer class="l-footer p-footer">
+<footer class="p-footer">
     <div class="l-inner">
-        footer.php
+        <div class="p-footer__content">
+            <div class="p-footer__left">
+                <div class="p-footer__logo">
+                    <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__home">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/footer_logo.png" alt="さくら司法書士事務所" width="431" height="38">
+                    </a>
+                </div>
+                <div class="p-footer__address">
+                    <p class="p-footer__address-text">
+                        〒188-0011 東京都西東京市田無町5-2-17-304 <a href="#">[地図]</a><br>
+                        平日 9:00-18:00（休日:土日祝）<br>
+                        代表司法書士：志村 理<br>
+                        東京司法書士会所属 第3974号
+                    </p>
+                </div>
+                <div class="p-footer__small">
+                    <small>© 2025 さくら司法書士事務所.</small>
+                </div>
+
+            </div>
+            <div class="p-footer__right">
+                <ul class="p-footer__lists">
+                    <li class="p-footer__list">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">ホーム</a>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">法務サポート</a>
+                        <ul class="p-footer__subLists">
+                            <li class="p-footer__subList">
+                                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__subLink">遺産相続</a>
+                            </li>
+                            <li class="p-footer__subList">
+                                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__subLink">相続の知識</a>
+                            </li>
+                            <li class="p-footer__subList">
+                                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__subLink">遺言</a>
+                            </li>
+                            <li class="p-footer__subList">
+                                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__subLink">不動産登記</a>
+                            </li>
+                            <li class="p-footer__subList">
+                                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__subLink">成年後見</a>
+                            </li>
+                            <li class="p-footer__subList">
+                                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__subLink">その他業務</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">料金表</a>
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">事務所案内</a>
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">司法書士紹介</a>
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">よくある質問</a>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">所長ブログ</a>
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">個人情報保護方針</a>
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__link">お問い合わせ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
