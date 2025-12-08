803b804e04e9a2a2cd4589d1a13d458008f4adb4
yumauda <firststep-yumauda@gmail.com>
2025-12-08 21:56:19 +0900

    相続ページ下層コンテンツ

--- page-inheritance.php
+++ page-inheritance.php
@@ -78,6 +78,50 @@
               <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/inheritance_illustration.jpg" alt="遺産相続のイラスト" width="390" height="260">
             </figure>
           </div>
+          <div class="p-inheritance__example">
+            <h4 class="p-inheritance__example-title">遺産の例</h4>
+            <div class="p-inheritance__example-box">
+              <ol class="p-inheritance__example-list p-inheritance__example-list--left">
+                <li class="p-inheritance__example-item">現金や預貯金</li>
+                <li class="p-inheritance__example-item">土地や建物、マンション、別荘などの不動産</li>
+                <li class="p-inheritance__example-item">株式（有価証券）や投資信託</li>
+                <li class="p-inheritance__example-item">自動車や貴金属、絵画などの動産</li>
+                <li class="p-inheritance__example-item">賃借権・特許権・著作権などの権利</li>
+                <li class="p-inheritance__example-item">借入金や未払い金などの債務</li>
+              </ol>
+            </div>
+            <div class="p-inheritance__attention-text">
+              <p class="p-inheritance__text">このように、<span class="p-inheritance__text--bold">亡くなった人の残した財産を特定の人が引き継ぐ（もらう）ことを</span><span class="p-inheritance__text--attention">「遺産相続」</span>と言います。</p>
+            </div>
+          </div>
+          <div class="p-inheritance__section mt87">
+            <h3 class="c-page-title">遺産の相続方法</h3>
+            <div class="p-inheritance__text-wrapper">
+              <p class="p-inheritance__text">遺産相続の方法としては、主に次のような方法があります。</p>
+            </div>
+            <div class="p-inheritance__number">
+              <ul class="p-inheritance__number-lists">
+                <li class="p-inheritance__number-list">
+                  <span>法定相続：</span>民法で決められた人が決められた分だけもらう相続
+                </li>
+                <li class="p-inheritance__number-list">
+                  <span>遺言による相続：</span>亡くなった人が遺言書により相続の内容を決める相続
+                </li>
+                <li class="p-inheritance__number-list">
+                  <span>遺産分割協議による相続：</span>相続人全員で協議して遺産の分割方法を決める相続
+                </li>
+              </ul>
+
+            </div>
+            <div class="p-inheritance__text-wrapper">
+              <p class="p-inheritance__text">被相続人（亡くなった人）が遺言を残している場合は、原則、この遺言書に沿って遺産は相続されます。</p>
+              <p class="p-inheritance__text">一方、遺言書がない場合には、民法で定められた方法に沿って遺産を相続することになりますが、相続人全員で話し合って、民法で定められた分け方とは異なる内容で遺産を相続することもできます。</p>
+              <p class="p-inheritance__text">親（父母）や夫、妻など近い親族が亡くなり<span class="p-inheritance__text--bold">相続が開始すると、遺産分割協議をはじめ、預貯金の解約、相続放棄、不動産の名義変更（相続登記）などの</span>遺産相続…様々な相続手続が待っており、なかなか悲しんでもいられないのが現実です。</p>
+            </div>
+
+
+          </div>
+
         </div>
 
         <!-- サイドバー -->
