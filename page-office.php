93e773d358f0f43b66cbdb820c991bea79cf0ef3
yumauda <firststep-yumauda@gmail.com>
2025-12-16 15:44:32 +0900

    司法書士紹介ページ

---
+++ page-office.php
@@ -0,0 +1,340 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <figure class="p-page-mv__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_bg.webp" alt="" width="1500" height="150">
+        </figure>
+        <h2 class="p-page-mv__title">事務所案内</h2>
+
+      </div>
+    </div>
+  </section>
+  <div class="p-breadcrumbs">
+    <div class="l-inner">
+      <div class="p-breadcrumbs__content">
+        <ul class="p-breadcrumbs__lists">
+          <li class="p-breadcrumbs__list">
+            <a href="#" class="p-breadcrumbs__link">
+              ホーム
+            </a>
+          </li>
+          <li class="p-breadcrumbs__list">
+            <div class="p-breadcrumbs__icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 6 10"
+                xmlns:xlink="http://www.w3.org/1999/xlink">
+                <path fill-rule="evenodd" fill="rgb(0, 0, 0)"
+                  d="M4.873,4.999 L0.241,9.563 L0.683,9.999 L5.758,4.999 L0.683,-0.001 L0.241,0.435 L4.873,4.999 Z" />
+              </svg>
+            </div>
+          </li>
+          <li class="p-breadcrumbs__list">
+            <a href="#" class="p-breadcrumbs__link">
+              事務所案内
+            </a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+
+  <section class="p-inheritance">
+    <div class="l-inner">
+      <div class="p-inheritance__container">
+        <!-- メインコンテンツ -->
+        <section class="p-price">
+          <div class="p-price__content">
+            <h3 class="c-page-title">事務所概要</h3>
+            <div class="p-price__block">
+              <table class="p-price__table p-price__table--half">
+                <tbody>
+                  <tr>
+                    <td>事務所名称</td>
+                    <td>さくら司法書士事務所</td>
+                  </tr>
+                  <tr>
+                    <td>代表司法書士</td>
+                    <td>
+                      芝村 望（しばむら のぞむ）<br>
+                      司法書士登録番号 東京第5074号<br>
+                      簡裁訴訟代理等認定者 第501103号<br>
+                      <a href="#" class="p-price__link">司法書士の紹介はこちら</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>所在地</td>
+                    <td>〒188-0011 東京都西東京市田無町5-2-17 コーカ・レナージュ304号</td>
+                  </tr>
+                  <tr>
+                    <td>電話番号</td>
+                    <td>042-469-3092</td>
+                  </tr>
+                  <tr>
+                    <td>営業時間</td>
+                    <td>
+                      平日：9時〜18時<br>
+                      土日祝日：休<br>
+                      メール相談：24時間受付<br>
+                      電話相談：平日9時〜18時
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>対応地域</td>
+                    <td>
+                      西東京市（田無）、小平市（花小金井）、清瀬市、東村山市、東久留米市、新座市、<br>
+                      練馬区、武蔵野市、埼玉県新座市、所沢市など。それ以外の地域の方でも一度ご相談ください。
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+          </div>
+          <div class="p-price__content">
+            <h3 class="c-page-title">アクセス</h3>
+            <div class="p-price__text-wrapper">
+              <p class="c-main-text">西武新宿線「田無駅」より徒歩4分。<br>
+                駐車場は完備しておりませんのでご了承下さい。（付近に有料パーキングが多数あります）</p>
+            </div>
+            <div class="p-price__map">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.9032382444707!2d139.5348474757898!3d35.728598172572035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018ef44df5e7faf%3A0x16ba133c888bf44!2z44GV44GP44KJ5Y-45rOV5pu45aOr5LqL5YuZ5omA!5e0!3m2!1sja!2sjp!4v1765526040917!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="p-price__btn-wrapper">
+              <a href="#" class="p-price__btn">
+                <p class="p-price__btn-text">Google map</p>
+                <div class="p-price__btn-icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 12 12">
+                    <path fill-rule="evenodd" fill="rgb(238, 129, 157)"
+                      d="M5.683,-0.002 L4.684,0.948 L9.290,5.326 L-0.001,5.326 L-0.001,6.669 L9.290,6.669 L4.684,11.046 L5.683,11.997 L11.997,5.997 L5.683,-0.002 Z" />
+                  </svg>
+                </div>
+              </a>
+            </div>
+
+
+          </div>
+        </section>
+
+        <!-- サイドバー -->
+        <aside class="p-inheritance__sidebar p-sidebar">
+          <div class="p-sidebar__menu">
+            <h3 class="p-sidebar__title">法務サポート</h3>
+            <ul class="p-sidebar__menu-list">
+              <li class="p-sidebar__menu-item p-sidebar__menu-item--parent">
+                <button class="p-sidebar__menu-toggle" type="button">
+                  <p class="p-sidebar__menu-toggleText">遺産相続</p>
+                  <span class="p-sidebar__menu-icon"></span>
+                </button>
+                <ul class="p-sidebar__submenu">
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance" class="p-sidebar__submenu-link">遺産相続</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-property" class="p-sidebar__submenu-link">遺産相続財産</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-flow" class="p-sidebar__submenu-link">遺産相続財産の流れ</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#heir" class="p-sidebar__submenu-link">相続権者</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="p-sidebar__menu-item p-sidebar__menu-item--parent">
+                <button class="p-sidebar__menu-toggle" type="button">
+                  <p class="p-sidebar__menu-toggleText">遺産の知識</p>
+                  <span class="p-sidebar__menu-icon"></span>
+                </button>
+                <ul class="p-sidebar__submenu">
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance" class="p-sidebar__submenu-link">遺産相続</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-property" class="p-sidebar__submenu-link">遺産相続財産</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-flow" class="p-sidebar__submenu-link">遺産相続財産の流れ</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#heir" class="p-sidebar__submenu-link">相続権者</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="p-sidebar__menu-item p-sidebar__menu-item--parent">
+                <button class="p-sidebar__menu-toggle" type="button">
+                  <p class="p-sidebar__menu-toggleText">遺言</p>
+                  <span class="p-sidebar__menu-icon"></span>
+                </button>
+                <ul class="p-sidebar__submenu">
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance" class="p-sidebar__submenu-link">遺産相続</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-property" class="p-sidebar__submenu-link">遺産相続財産</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-flow" class="p-sidebar__submenu-link">遺産相続財産の流れ</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#heir" class="p-sidebar__submenu-link">相続権者</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="p-sidebar__menu-item p-sidebar__menu-item--parent">
+                <button class="p-sidebar__menu-toggle" type="button">
+                  <p class="p-sidebar__menu-toggleText">不動産登記</p>
+                  <span class="p-sidebar__menu-icon"></span>
+                </button>
+                <ul class="p-sidebar__submenu">
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance" class="p-sidebar__submenu-link">遺産相続</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-property" class="p-sidebar__submenu-link">遺産相続財産</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-flow" class="p-sidebar__submenu-link">遺産相続財産の流れ</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#heir" class="p-sidebar__submenu-link">相続権者</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="p-sidebar__menu-item p-sidebar__menu-item--parent">
+                <button class="p-sidebar__menu-toggle" type="button">
+                  <p class="p-sidebar__menu-toggleText">成年後見</p>
+                  <span class="p-sidebar__menu-icon"></span>
+                </button>
+                <ul class="p-sidebar__submenu">
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance" class="p-sidebar__submenu-link">遺産相続</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-property" class="p-sidebar__submenu-link">遺産相続財産</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#inheritance-flow" class="p-sidebar__submenu-link">遺産相続財産の流れ</a>
+                  </li>
+                  <li class="p-sidebar__submenu-item">
+                    <a href="#heir" class="p-sidebar__submenu-link">相続権者</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="p-sidebar__menu-item">
+                <a href="#other-contracts" class="p-sidebar__menu-link">その他業務</a>
+              </li>
+              <li class="p-sidebar__menu-item">
+                <a href="#price-list" class="p-sidebar__menu-link">料金表</a>
+              </li>
+            </ul>
+            <div class="p-sidebar__slider-wrapper">
+              <div class="swiper slider-sidebar">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                  <!-- Slides -->
+                  <div class="swiper-slide">
+                    <a href="#" class="p-sidebar__slide-link">
+                      <figure class="p-sidebar__slide-img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_01.webp" alt="遺産整理" width="180" height="180">
+                      </figure>
+                    </a>
+                  </div>
+                  <div class="swiper-slide">
+                    <a href="#" class="p-sidebar__slide-link">
+                      <figure class="p-sidebar__slide-img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_02.webp" alt="遺産整理" width="180" height="180">
+                      </figure>
+                    </a>
+                  </div>
+                  <div class="swiper-slide">
+                    <a href="#" class="p-sidebar__slide-link">
+                      <figure class="p-sidebar__slide-img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_03.webp" alt="遺産整理" width="180" height="180">
+                      </figure>
+                    </a>
+                  </div>
+                  <div class="swiper-slide">
+                    <a href="#" class="p-sidebar__slide-link">
+                      <figure class="p-sidebar__slide-img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_04.webp" alt="遺産整理" width="180" height="180">
+                      </figure>
+                    </a>
+                  </div>
+                  <div class="swiper-slide">
+                    <a href="#" class="p-sidebar__slide-link">
+                      <figure class="p-sidebar__slide-img">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_05.webp" alt="遺産整理" width="180" height="180">
+                      </figure>
+                    </a>
+                  </div>
+
+                </div>
+                <!-- If we need pagination -->
+                <div class="swiper-pagination"></div>
+
+                <!-- If we need navigation buttons -->
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+
+                <!-- If we need scrollbar -->
+                <div class="swiper-scrollbar"></div>
+              </div>
+            </div>
+            <!-- Slider main container -->
+            <h3 class="p-sidebar__title mt30">その他ページ</h3>
+            <ul class="p-sidebar__menu-list">
+              <li class="p-sidebar__menu-item">
+                <a href="#other-contracts" class="p-sidebar__menu-link">事務所案内</a>
+              </li>
+              <li class="p-sidebar__menu-item">
+                <a href="#price-list" class="p-sidebar__menu-link">司法書士紹介</a>
+              </li>
+              <li class="p-sidebar__menu-item">
+                <a href="#price-list" class="p-sidebar__menu-link">よくある質問</a>
+              </li>
+              <li class="p-sidebar__menu-item">
+                <a href="#price-list" class="p-sidebar__menu-link">所長ブログ</a>
+              </li>
+              <li class="p-sidebar__menu-item">
+                <a href="#price-list" class="p-sidebar__menu-link">個人情報保護方針</a>
+              </li>
+            </ul>
+            <h3 class="p-sidebar__title mt30">アクセス</h3>
+            <div class="p-sidebar__map-wrapper">
+              <div class="p-sidebar__map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.9032382444707!2d139.5348474757898!3d35.728598172572035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018ef44df5e7faf%3A0x16ba133c888bf44!2z44GV44GP44KJ5Y-45rOV5pu45aOr5LqL5YuZ5omA!5e0!3m2!1sja!2sjp!4v1765526040917!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              </div>
+              <div class="p-sidebar__map-detail">
+                <p class="p-sidebar__map-title">さくら司法書士事務所</p>
+                <p class="p-sidebar__map-text">東京都西東京市田無町5-2-17-304[<a href="#">地図</a>]<br>田無駅北口より徒歩4分</p>
+              </div>
+              <ul class="p-sidebar__menu-list">
+                <li class="p-sidebar__menu-item">
+                  <a href="#other-contracts" class="p-sidebar__menu-link">アクセス</a>
+                </li>
+
+              </ul>
+
+            </div>
+
+
+          </div>
+        </aside>
+      </div>
+    </div>
+  </section>
+
+
+  <?php get_template_part('includes/contact'); ?>
+
+</main>
+<?php get_footer() ?>
